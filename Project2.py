6a92ad16be59de58a9d5eb4624404dbd1d14e829
JOKE <Jcabezas2204@gmail.com>
2026-06-25 18:03:37 -0300

    cargar y guardar datos

--- Project2.py
+++ Project2.py
@@ -1,44 +1,92 @@
 #Trabajo en equipo 
 
-import json
 catalogo={}
 usuarios={}
 prestamos=[]
-ruta="Biblioteca.json"
+ruta="Biblioteca.txt"
 
 #guardar_datos
 def guardar_datos(catalogo, usuarios, prestamos, ruta):
-    datos={
+    archivo = open(ruta, "w")
+
+    for isbn in catalogo:
+        libro = catalogo[isbn]
+        archivo.write("libro;" +
+            libro["isbn"] + ";" +
+            libro["titulo"] + ";" +
+            libro["autor"] + ";" +
+            libro["genero"] + ";" +
+            str(libro["ejemplares_totales"]) + ";" +
+            str(libro["ejemplares_disponibles"]) + "\n")
+
+    for numero in usuarios:
+        usuario = usuarios[numero]
+        activos = ",".join(usuario["prestamos_activos"])
+        archivo.write("usuario;" +
+            str(usuario["numero_socio"]) + ";" +
+            usuario["nombre"] + ";" +
+            activos + "\n")
+
+    for prestamo in prestamos:
+        archivo.write("prestamo;" +
+            str(prestamo["numero_socio"]) + ";" +
+            prestamo["isbn"] + ";" +
+            prestamo["fecha_prestamo"] + ";" +
+            prestamo["fecha_limite"] + ";" +
+            str(prestamo["devuelto"]) + "\n")
+
+    archivo.close()
+
+#cargar_datos(ruta)
+def cargar_datos(ruta):
+    archivo = open(ruta, "r")
+    for linea in archivo:
+        lista = linea.strip().split(";")
+        if lista[0] == "libro":
+            catalogo[lista[1]] = {
+                "isbn": lista[1],
+                "titulo": lista[2],
+                "autor": lista[3],
+                "genero": lista[4],
+                "ejemplares_totales": int(lista[5]),
+                "ejemplares_disponibles": int(lista[6])
+            }
+        elif lista[0] == "usuario":
+            if lista[3] == "":
+                activos = []
+            else:
+                activos = lista[3].split(",")
+            usuarios[lista[1]] = {
+                "numero_socio": lista[1],
+                "nombre": lista[2],
+                "prestamos_activos": activos
+            }
+        elif lista[0] == "prestamo":
+            prestamos.append({
+                "numero_socio": lista[1],
+                "isbn": lista[2],
+                "fecha_prestamo": lista[3],
+                "fecha_limite": lista[4],
+                "devuelto": lista[5] == "True"
+            })
+    archivo.close()
+    datos = {
         "catalogo": catalogo,
         "usuarios": usuarios,
         "prestamos": prestamos
-        }
-    with open(ruta, "w") as archivo:
-        json.dump(datos, archivo, indent=4)
-    
-#cargar_datos(ruta)
-def cargar_datos(ruta):
-    with open(ruta, "r") as archivo:
-        datos = json.load(archivo)
+    }
     return datos
-    
+
 try:
-
     datos = cargar_datos(ruta)
-
     catalogo = datos["catalogo"]
-
     usuarios = datos["usuarios"]
-
     prestamos = datos["prestamos"]
-
+    
 except:
-
     pass
-
 def menu_principal():
     while True:
-
         print("Seleccione:")
         print("1. Agregar libro")
         print("2. Eliminar libro")
@@ -51,37 +99,26 @@ def menu_principal():
         print("9. Listar vencidos")
         print("0. Salir")
         opcion = input("Opción: ")
-
         if opcion == "1":
             agregar_libro()
-
         elif opcion == "2":
             eliminar_libro()
-
         elif opcion == "3":
             buscar_libro()
-
         elif opcion == "4":
             registrar_usuario()
-
         elif opcion == "5":
             dar_baja_usuario()
-
         elif opcion == "6":
             historial_usuario()
-
         elif opcion == "7":
             registrar_prestamo()
-
         elif opcion == "8":
             registrar_devolucion()
-
         elif opcion == "9":
             listar_vencidos()
-
         elif opcion == "0":
             print("Programa finalizado")
-
             break
 
         else:
@@ -92,9 +129,7 @@ def menu_principal():
 def agregar_libro():
     isbn = input("isbn: ")
     if isbn in catalogo:
-        print(f"El isnb", (isbn),"ya existe")
-        return
-
+        raise Exception("El isbn ya existe")
     libro = {
         "isbn": isbn,
         "titulo": input("Título: "),
@@ -112,25 +147,19 @@ def agregar_libro():
 #eliminar_libro
 def eliminar_libro():
     isbn = input("isbn a eliminar: ")
-    
+
     if isbn not in catalogo:
         print("El libro no existe")
         return
-    
+
     for prestamo in prestamos:
         if prestamo["isbn"] == isbn and not prestamo["devuelto"]:
             print("El libro tiene préstamos activos")
             return
 
     del catalogo[isbn]
-    guardar_datos(
-        catalogo,
-        usuarios,
-        prestamos,
-        ruta
-    )
+    guardar_datos(catalogo, usuarios, prestamos, ruta)
     print("Libro eliminado")
-
 #buscar_libro
 def buscar_libro():
     termino = input("Buscar: ").lower()
@@ -161,7 +190,7 @@ def registrar_usuario():
     usuarios[numero_socio] = usuario
     guardar_datos(catalogo, usuarios, prestamos, ruta)
     print("Usuario agregado")
-    return usuarios	
+    return usuarios
 
 #dar_baja_usuario
 def dar_baja_usuario():
